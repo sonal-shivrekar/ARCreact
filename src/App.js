50be840c45998172ca3358f803f858a850e82e92
sonal-shivrekar <sonalshivrekar8080@gmail.com>
2023-05-22 17:59:49 +0530

    8 commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add/Add.js'
+import{Sub,Multi} from './components/Sub/Sub.js'
 
 function App() {
   return (
@@ -13,6 +14,10 @@ function App() {
     <a href = "https://www.google.com/">google</a>
 
 <Add a = "2" b = "4"></Add>
+
+<Sub m = "5" n ="8"></Sub>
+
+<Multi o = "8" k = "6"></Multi>
 
 </div>
   )
---
+++ src/components/Sub/Sub.js
@@ -0,0 +1,23 @@
+export const Sub =(props) =>{
+    return(
+
+        <div>
+            <h1>Sub component</h1>
+            <p>Sub = {parseInt(props.m) - parseInt(props.n)}</p>
+
+        </div>
+        
+    )
+}
+
+export const Multi =(props) =>{
+
+    return(
+
+        <div>
+            <h1>Multi component</h1>
+
+            <p>Multi = {parseInt(props.o) * parseInt(props.k)}</p>
+        </div>
+    )
+}
